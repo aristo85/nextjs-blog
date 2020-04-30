2039ead4e8d8e83fa91e67becbf2e1847a7519b2
aristo85 <aritomsk@yahoo.com>
2020-04-30 23:52:29 +0700

    axios

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env:{
         MONGO_URI:"mongodb+srv://aristo85:123456Abc@cluster0-ehted.mongodb.net/test?retryWrites=true&w=majority",
-        URI:"https://nextjs-blog-iota-dun.now.sh/"
+        URI:"https://nextjs-blog-iota-dun.now.sh/api/notes"
     }
 }
