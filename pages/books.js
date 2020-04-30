38b417b73643a075e8abfad46f0f142954238ce9
aristo85 <aritomsk@yahoo.com>
2020-05-01 00:05:24 +0700

    axios

--- pages/books.js
+++ pages/books.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export async function getStaticProps() {
-    const res = await axios.get('/api/notes');
+    const res = await axios.get('http://localhost:3000/api/notes');
     const { data } = await res;
     console.log(data);
     return {
