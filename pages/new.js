ebfd083c77e6629fdbe7ba1bf23d6799e14b279c
aristo85 <aritomsk@yahoo.com>
2020-05-01 14:51:19 +0700

    attempt17

--- pages/new.js
+++ pages/new.js
@@ -1,86 +1,85 @@
 import React, {Component} from 'react';
-// import Link from "next/link";
-// import axios from 'axios';
+import axios from 'axios';
 import {withRouter} from "next/router";
-// import {Form, Spinner, FormGroup, Input, Label, Col, Button, FormFeedback} from "reactstrap";
-// import { string, object} from "yup";
+import {Form, Spinner, FormGroup, Input, Label, Col, Button, FormFeedback} from "reactstrap";
+import { string, object} from "yup";
 
-// let yup = require('yup');
-//
-// const schema = yup.object().shape({
-//     title: yup.string().required().max(20),
-//     description: yup.string().required().max(100)
-// });
+let yup = require('yup');
+
+const schema = yup.object().shape({
+    title: yup.string().required().max(20),
+    description: yup.string().required().max(100)
+});
 
 class New extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         title: '',
-    //         description: '',
-    //         errors: [],
-    //         isSubmitting: false,
-    //     }
-    // }
-    //
-    // handleChang = (e) => {
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     });
-    // };
-    // handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     let dataToSubmit = {title: this.state.title, description: this.state.description};
-    //     schema.validate(dataToSubmit, {abortEarly: false}).then(valid => {
-    //         if(valid){
-    //             this.setState({errors: [], isSubmitting: true});
-    //             this.createNote(dataToSubmit);
-    //         }
-    //     }).catch(err => {
-    //         console.log(err.errors[0])
-    //
-    //         this.setState({errors: [], isSubmitting: false});
-    //         this.setState({errors: this.state.errors.concat(err.errors)});
-    //     })
-    // };
-    //
-    // createNote = async (data) => {
-    //     try {
-    //         const res = await axios.post('http://localhost:3000/api/notes', data);
-    //         this.props.router.push('/')
-    //     }catch (e) {
-    //
-    //     }
-    // };
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            description: '',
+            errors: [],
+            isSubmitting: false,
+        }
+    }
+
+    handleChang = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let dataToSubmit = {title: this.state.title, description: this.state.description};
+        schema.validate(dataToSubmit, {abortEarly: false}).then(valid => {
+            if(valid){
+                this.setState({errors: [], isSubmitting: true});
+                this.createNote(dataToSubmit);
+            }
+        }).catch(err => {
+            console.log(err.errors[0])
+
+            this.setState({errors: [], isSubmitting: false});
+            this.setState({errors: this.state.errors.concat(err.errors)});
+        })
+    };
+
+    createNote = async (data) => {
+        try {
+            const res = await axios.post(`${process.env.URI}/api/notes`, data);
+            this.props.router.push('/')
+        }catch (e) {
+
+        }
+    };
 
     render() {
         return (
             <div className="container">
                 <h1>Create Note</h1>
-                {/*<div>*/}
-                {/*    {*/}
-                {/*        this.state.isSubmitting*/}
-                {/*            ? <Spinner color="success" className="spinner" />*/}
-                {/*            :<Form onSubmit={this.handleSubmit}>*/}
-                {/*                <FormGroup row>*/}
-                {/*                    <Label sm={2}>Title</Label>*/}
-                {/*                    <Col sm={8}>*/}
-                {/*                        <Input type="text" name="title" placeholder="type here" onChange={this.handleChang} />*/}
-                {/*                    </Col>*/}
-                {/*                </FormGroup>*/}
-                {/*                <FormGroup row>*/}
-                {/*                    <Label sm={2}>Description</Label>*/}
-                {/*                    <Col sm={8}>*/}
-                {/*                        <Input type="textarea" name="description" placeholder="type here" onChange={this.handleChang} />*/}
-                {/*                    </Col>*/}
-                {/*                </FormGroup>*/}
-                {/*                {this.state.errors.length > 0*/}
-                {/*                    ? this.state.errors.map(e => <li className="errors">{e}</li>) : null}*/}
-                {/*                <Button type="submit">Create</Button>*/}
+                <div>
+                    {
+                        this.state.isSubmitting
+                            ? <Spinner color="success" className="spinner" />
+                            :<Form onSubmit={this.handleSubmit}>
+                                <FormGroup row>
+                                    <Label sm={2}>Title</Label>
+                                    <Col sm={8}>
+                                        <Input type="text" name="title" placeholder="type here" onChange={this.handleChang} />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label sm={2}>Description</Label>
+                                    <Col sm={8}>
+                                        <Input type="textarea" name="description" placeholder="type here" onChange={this.handleChang} />
+                                    </Col>
+                                </FormGroup>
+                                {this.state.errors.length > 0
+                                    ? this.state.errors.map(e => <li className="errors">{e}</li>) : null}
+                                <Button type="submit">Create</Button>
 
-                {/*            </Form>*/}
-                {/*    }*/}
-                {/*</div>*/}
+                            </Form>
+                    }
+                </div>
             </div>
         );
     }
