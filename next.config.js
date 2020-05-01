e75cbd7a4afb1ac5b5587f0ef9e2984634531bc0
aristo85 <aritomsk@yahoo.com>
2020-05-01 09:15:59 +0700

    again3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     env:{
-        MONGO_URI:"mongodb+srv://aristo85:123456Abc@cluster0-ehted.mongodb.net/test?retryWrites=true&w=majority",
-        URI:"http://localhost:3000/api/notes"
+        MONGO_URI:"mongodb+srv://aristo85:123456Abc@cluster0-ehted.mongodb.net/test?retryWrites=true&w=majority"
     }
 }
