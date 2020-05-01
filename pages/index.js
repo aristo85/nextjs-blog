ebcca35cb3200abf623334e18406f149e0c7fa6c
aristo85 <aritomsk@yahoo.com>
2020-05-01 11:01:31 +0700

    again6

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-
+import Link from "next/link";
+import { Card, CardBody, CardTitle, Button, Row, Col } from "reactstrap";
 import axios from "axios";
 
 export async function getStaticProps() {
@@ -7,17 +8,42 @@ export async function getStaticProps() {
     console.log(data.data);
     return {
         props: {
-            data: data.data
+            notes: data.data
         },
     }
 }
 
-export default function Home({data}) {
+export default function Index({notes}) {
     return (
-        <div>
-            {data.map(book => (
-                <li key={book._id}>{book.title}</li>
-            ))}
-        </div>
+       <div className="container">
+        <h1>Notes</h1>
+    <Row className="mt-5">
+        {notes.map(note => {
+            return (
+                <Col sm="6" key={note._id} className="mt-2 text-center">
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                {note.title}
+                            </CardTitle>
+                            <Row>
+                                <Col xs="6">
+                                    {/*<Link href={`/${note._id}`}>*/}
+                                        <Button block color="primary">View</Button>
+                                    {/*</Link>*/}
+                                </Col>
+                                <Col>
+                                    {/*<Link href={`/${note._id}/edit`}>*/}
+                                        <Button block color="primary">Edit</Button>
+                                    {/*</Link>*/}
+                                </Col>
+                            </Row>
+                        </CardBody>
+                    </Card>
+                </Col>
+            )
+        })}
+    </Row>
+    </div>
     )
 }
