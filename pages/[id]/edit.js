3f199c36df277acfe9400da43464f836bb3a56fe
aristo85 <aritomsk@yahoo.com>
2020-05-01 15:52:12 +0700

    attempt20

--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -99,18 +99,18 @@ class Edit extends Component {
     }
 }
 
-export default withRouter(Edit);
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id (paths = [{params: {id: 'ssg-ssr'}}, {params: {id: 'pre-rendering'}}])
-    const paths = await getAllPaths();
-    // const paths = [{params: {id: 'ssg-ssr'}}, {params: {id: 'pre-rendering'}}]
-    return {
-        paths,
-        fallback: false
-    }
-}
-export async function getStaticProps({ params }) {
+// export default withRouter(Edit);
+//
+// export async function getStaticPaths() {
+//     // Return a list of possible value for id (paths = [{params: {id: 'ssg-ssr'}}, {params: {id: 'pre-rendering'}}])
+//     const paths = await getAllPaths();
+//     // const paths = [{params: {id: 'ssg-ssr'}}, {params: {id: 'pre-rendering'}}]
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
+export async function getServerSideProps({ params }) {
     // Fetch necessary data for the blog post using params.id
     const { data } = await getNoteById(params.id);
     return {props: {data}}
