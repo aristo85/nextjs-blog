b653c56a338df653d91ab58f91c9ddd6151156e1
aristo85 <aritomsk@yahoo.com>
2020-05-02 21:34:24 +0700

    next1

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,49 @@
 import Link from "next/link";
 import { Card, CardBody, CardTitle, Button, Row, Col } from "reactstrap";
 import { getAllNotes } from "../store/allNotes";
-import axios from "axios";
 
-
-
-// export async function getStaticProps() {
-//     const notes = await getAllNotes();
-//     return {
-//         props: {
-//             notes
-//         },
-//     }
-// }
+export async function getServerSideProps() {
+    const notes = await getAllNotes();
+    return {
+        props: {
+            notes
+        },
+    }
+}
 
 const Index = ({notes}) => {
     return (
-       <div className="container">
-        <h1>Notes</h1>
-    <Row className="mt-5">
-        {notes.map(note => {
-            return (
-                <Col sm="6" key={note._id} className="mt-2 text-center">
-                    <Card>
-                        <CardBody>
-                            <CardTitle>
-                                {note.title}
-                            </CardTitle>
-                            <Row>
-                                <Col xs="6">
-                                    {/*<Link href="/[id]" as={`/${note._id}`}>*/}
-                                        <Button block color="primary">View</Button>
-                                    {/*</Link>*/}
-                                </Col>
-                                <Col>
-                                    {/*<Link href="/[id]/edit" as={`/${note._id}/edit`}>*/}
-                                        <Button block color="primary">Edit</Button>
-                                    {/*</Link>*/}
-                                </Col>
-                            </Row>
-                        </CardBody>
-                    </Card>
-                </Col>
-            )
-        })}
-    </Row>
-    </div>
+        <div className="container">
+            <h1>Notes</h1>
+            <Row className="mt-5">
+                {notes.map(note => {
+                    return (
+                        <Col sm="6" key={note._id} className="mt-2 text-center">
+                            <Card>
+                                <CardBody>
+                                    <CardTitle>
+                                        {note.title}
+                                    </CardTitle>
+                                    <Row>
+                                        <Col xs="6">
+                                            {/*<Link href="/[id]" as={`/${note._id}`}>*/}
+                                            <Button block color="primary">View</Button>
+                                            {/*</Link>*/}
+                                        </Col>
+                                        <Col>
+                                            {/*<Link href="/[id]/edit" as={`/${note._id}/edit`}>*/}
+                                            <Button block color="primary">Edit</Button>
+                                            {/*</Link>*/}
+                                        </Col>
+                                    </Row>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
+        </div>
     )
-}
+};
 
 export default Index;
-
-Index.getInitialProps = async () => {
-    const res = await axios.get(`https://morning-springs-53145.herokuapp.com/api/notes`);
-    const { data } = await res.data;
-    return { notes: data }
-}
--- pages/new.js
+++ pages/new.js
@@ -1,12 +1,8 @@
-// import Link from "next/link";
-// import { useState, useEffect } from 'react';
 import React, {Component} from 'react';
 import axios from 'axios';
-// import fetch from 'isomorphic-unfetch';
-// import { useRouter } from "next/router";
 import {Form, Spinner, FormGroup, Input, Label, Col, Button, FormFeedback} from "reactstrap";
-import { string, object} from "yup";
 import {withRouter} from "next/router";
+import {getAllNotes} from "../store/allNotes";
 
 let yup = require('yup');
 
@@ -15,97 +11,14 @@ const schema = yup.object().shape({
     description: yup.string().required().max(100)
 });
 
-// const NewNote = () => {
-//     const [form, setForm] = useState({ title: '', description: ''});
-//     const [isSubmitting, setIsSubmitting] = useState(false);
-//     const [errors, setErrors] = useState({});
-//     const router = useRouter();
-//
-//     useEffect(() => {
-//        if (isSubmitting) {
-//            if (Object.keys(errors).length === 0) {
-//                createNote();
-//            } else {
-//                setIsSubmitting(false);
-//            }
-//        }
-//     }, [errors]);
-//
-//     const createNote = async () => {
-//         // let data = JSON.stringify(form);
-//         try {
-//             const res = await fetch(`${process.env.URI}/api/notes`, {
-//                 method: 'POST',
-//                 headers: {
-//                     "Accept": "application/json",
-//                     "Content-type": "application/json"
-//                 },
-//                 body: JSON.stringify(form)
-//             });
-//             await router.push('/');
-//         }catch (e) {
-//             console.log(e);
-//         }
-//     };
-//
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         validate();
-//         setIsSubmitting(true);
-//     };
-//     const handleChange = (e) => {
-//         setForm({
-//             ...form,
-//             [e.target.name]: e.target.value
-//         })
-//     };
-//
-//     const validate = () => {
-//         let err = {};
-//         let dataToSubmit = {title: form.title, description: form.description};
-//         schema.validate(dataToSubmit, {abortEarly: false}).then(valid => {
-//             if(valid){
-//                 setErrors(err);
-//             }
-//         }).catch(err => {
-//             setErrors(err);
-//         });
-//
-//     };
-//
-//     return (
-//         <div className="container">
-//             <h1>Create Note</h1>
-//             <div>
-//                 {
-//                     isSubmitting
-//                         ? <Spinner color="success" className="spinner" />
-//                         :<Form onSubmit={handleSubmit}>
-//                             <FormGroup row>
-//                                 <Label sm={2}>Title</Label>
-//                                 <Col sm={8}>
-//                                     <Input type="text" name="title" placeholder="type here" onChange={handleChange} />
-//                                 </Col>
-//                             </FormGroup>
-//                             <FormGroup row>
-//                                 <Label sm={2}>Description</Label>
-//                                 <Col sm={8}>
-//                                     <Input type="textarea" name="description" placeholder="type here" onChange={handleChange} />
-//                                 </Col>
-//                             </FormGroup>
-//                             {errors.length > 0
-//                                 ? errors.map(e => <li className="errors">{e}</li>) : null}
-//                             <Button type="submit">Create</Button>
-//
-//                         </Form>
-//                 }
-//             </div>
-//         </div>
-//     )
-// };
-// export default NewNote;
-
-
+export async function getStaticProps() {
+    const notes = await getAllNotes();
+    return {
+        props: {
+            notes
+        },
+    }
+}
 
 class New extends Component {
     constructor(props) {
@@ -129,21 +42,28 @@ class New extends Component {
         schema.validate(dataToSubmit, {abortEarly: false}).then(valid => {
             if(valid){
                 this.setState({errors: [], isSubmitting: true});
-                this.createNote(dataToSubmit);
+                // this.createNote(dataToSubmit);
             }
         }).catch(err => {
-            console.log(err.errors[0])
-
             this.setState({errors: [], isSubmitting: false});
             this.setState({errors: this.state.errors.concat(err.errors)});
         })
     };
 
-    createNote = async (data) => {
-        try {
-            const res = await axios.post(`https://morning-springs-53145.herokuapp.com/api/notes`, data);
-            this.props.router.push('/')
-        }catch (e) {
+    componentDidUpdate() {
+        this.createNote();
+    }
+
+    createNote = async () => {
+        if (this.state.isSubmitting) {
+            let dataToSubmit = {title: this.state.title, description: this.state.description};
+            try {
+                await axios.post('/api/notes', dataToSubmit);
+            }catch (e) {
+                console.log(e);
+            }
+            console.log('1');
+            await this.props.router.push('/');
 
         }
     };
@@ -170,7 +90,7 @@ class New extends Component {
                                     </Col>
                                 </FormGroup>
                                 {this.state.errors.length > 0
-                                    ? this.state.errors.map(e => <li className="errors">{e}</li>) : null}
+                                    ? this.state.errors.map(e => <li className="errors" key={e}>{e}</li>) : null}
                                 <Button type="submit">Create</Button>
 
                             </Form>
